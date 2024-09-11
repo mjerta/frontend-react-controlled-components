51603da80aa574167dc174b0baf86ed30851cd9d
Maarten Postma <maarten.postma1000@gmail.com>
2024-09-11 23:19:23 +0200

    Add react hook form

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,44 @@
 import React from 'react';
+import {useForm} from 'react-hook-form';
 import './App.css';
 
 function App() {
+
+  const {register, handleSubmit, formState: {errors}, watch} = useForm();
+
+  const watchSelectedReferrer = watch('found-through');
+  // const watchAllFields = watch();
+  // console.log("UserName:", watchAllFields.userName);
   // const [nameInput, setNameInput] = React.useState('');
   // const [ageInput, setAgeInput] = React.useState('0');
   // const [comments, setCommentInput] = React.useState('');
   // const [checkboxInput, toggleCommentInput] = React.useState(false);
 
   const [state, setState] = React.useState({
-    userName:'',
-    age:'0',
+    userName: '',
+    age: '0',
     comments: '',
     checkboxInput: false,
   });
 
-  function handleSubmit(e) {
-    e.preventDefault();
+  function onFormSubmit
+  (data) {
+    console.log(data);
+    console.log(data.userName, data.age, data.comments, data.checkboxInput);
     console.log("Verstuurd!")
     console.log(state.userName, state.age, state.comments, state.checkboxInput);
   }
 
+
   function handleChange(e) {
+    console.log(e.target.value)
     const newValue = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
     setState({...state, [e.target.name]: newValue});
   }
 
   return (
     <div className="wrapper">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onFormSubmit)}>
         <fieldset>
           <legend>Gegevens</legend>
           <label htmlFor="userName">
@@ -35,20 +46,36 @@ function App() {
             <input
               type="text"
               id="userName"
-              name="userName"
+              // this regist methode below is using a spread operator cause its need to put the reference as well as the name of the element
+              {...register("userName", {
+                // Input text when filled in will return to true
+                // required: "true",
+                required: "Naam is verplicht",
+                minLength: {
+                  value: 3,
+                  message: "Naam moet minimaal 3 karakters bevatten"
+                }
+              })}
               value={state.userName}
               onChange={handleChange}
             />
+            {errors.userName && <p>{errors.userName.message}</p>}
           </label>
           <label htmlFor="age">
             Leeftijd:
             <input
               type="number"
               id="age"
-              name="age"
+              {...register("age", {
+                max: {
+                  value: 80,
+                  message: "Je mag maximaal 80 jaar oud zijn"
+                },
+              })}
               value={state.age}
               onChange={handleChange}
             />
+            {errors.age && <p>{errors.age.message}</p>}
           </label>
         </fieldset>
         <fieldset>
@@ -58,18 +85,40 @@ function App() {
             <textarea
               id="comments"
               placeholder="Wat vond je van het recept"
-              name="comments"
+              {...register("comments", {
+                maxLength: {
+                  value: 50,
+                  message: "Comments mogen 50 karakters bevatten"
+                },
+              })}
               value={state.comments}
               onChange={handleChange}
               rows="4"
             >
                 </textarea>
+            {errors.comments && <p>{errors.comments.message}</p>}
           </label>
+          <label htmlFor="referrer">
+            Hoe heb je dit recept gevonden?
+            <select id="referrer" {...register("found-through")}>
+              <option value="google">Google</option>
+              <option value="friend">Vriend</option>
+              <option value="advertisement">Advertentie</option>
+              <option value="other">Anders</option>
+            </select>
+          </label>
+          {watchSelectedReferrer === "other" &&
+            <input
+              type="text"
+              {...register("found-through-anders")}
+            />
+
+          }
           <label htmlFor="newsletter">
             <input
               type="checkbox"
               id="newsletter"
-              name="checkboxInput"
+              {...register("checkboxInput")}
               checked={state.checkboxInput}
               onChange={handleChange}
             />
